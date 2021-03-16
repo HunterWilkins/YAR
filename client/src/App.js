f5c79687131b64f288e09b154e9e7596a3479647
HunterWilkins <wilkins.hunter@gmail.com>
2021-03-16 10:59:24 -0500

    Updating commits

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import "./reset.css";
+import "./basics.css";
 import "./style.css";
+import {Router, Link, Switch} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <header>
+        <h1>YAR</h1>
+        <h2>Young and Ready</h2>
+
+        <nav>
+          
+        </nav>
+      </header>
+
+      <main>
+        <article>
+          
+        </article>
+      </main>
     </div>
   );
 }
